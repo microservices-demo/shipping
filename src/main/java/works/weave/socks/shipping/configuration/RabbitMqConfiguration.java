118e50454f1d2e9a42f89d52a2b1c32afd831dae
Vishal Lal <vishal.lal@container-solutions.com>
2017-02-13 18:16:46 +0100

    Make the rabbitmq host configurable

--- src/main/java/works/weave/socks/shipping/configuration/RabbitMqConfiguration.java
+++ src/main/java/works/weave/socks/shipping/configuration/RabbitMqConfiguration.java
@@ -7,16 +7,22 @@ import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
 public class RabbitMqConfiguration {
+
     final static String queueName = "shipping-task";
+
+    @Value("${spring.rabbitmq.host}")
+    private String host;
 
     @Bean
     public ConnectionFactory connectionFactory() {
-        CachingConnectionFactory connectionFactory = new CachingConnectionFactory("rabbitmq");
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory(host);
         connectionFactory.setCloseTimeout(5000);
         connectionFactory.setConnectionTimeout(5000);
         connectionFactory.setUsername("guest");
